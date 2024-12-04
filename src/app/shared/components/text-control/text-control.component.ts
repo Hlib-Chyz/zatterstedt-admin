270c9b24b1c7b9773cbae1947172a6de52b20aa5
GlebChiz <gchyz@jsdaddy.io>
2024-12-04 20:04:22 +0200

    fix: text control - numeric, boolean

--- src/app/shared/components/text-control/text-control.component.ts
+++ src/app/shared/components/text-control/text-control.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject, input } from '@angular/core';
-import { ControlContainer, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, input, OnInit } from '@angular/core';
+import { ControlContainer, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ErrorMessagesComponent } from '@shared/components/error-messages/error-messages.component';
+import { startWith } from 'rxjs';
 
 @Component({
     selector: 'app-text-control',
@@ -13,7 +14,7 @@ import { ErrorMessagesComponent } from '@shared/components/error-messages/error-
         },
     ],
 })
-export class TextControlComponent {
+export class TextControlComponent implements OnInit {
     private readonly controlContainer = inject(ControlContainer, { optional: true });
     public readonly label = input.required<string>();
     public readonly controlName = input.required<string>();
@@ -23,5 +24,30 @@ export class TextControlComponent {
 
     public get parentFormGroup(): FormGroup {
         return this.controlContainer?.control as FormGroup;
+    }
+
+    public get control(): FormControl {
+        return this.parentFormGroup?.get(this.controlName()) as FormControl;
+    }
+
+    public ngOnInit(): void {
+        if (this.control) {
+            this.control.valueChanges.pipe(startWith(this.control.value)).subscribe((value) => {
+                if (value !== null && value !== void 0) {
+                    let transformedValue = value;
+                    switch (this.type()) {
+                        case 'number':
+                            transformedValue = Number(value);
+                            break;
+                        case 'checkbox':
+                            transformedValue = value === 'true' || value === true;
+                            break;
+                    }
+                    if (transformedValue !== value) {
+                        this.control.setValue(transformedValue, { emitEvent: false });
+                    }
+                }
+            });
+        }
     }
 }
